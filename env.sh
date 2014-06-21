a710f37cf9df061422a2daac5d7e27db38ff5086
Vladyslav Libov <vladyslav@linux-mebx>
2014-06-21 12:10:15 +0200

    removed unnecessary code

--- env.sh
+++ env.sh
@@ -1,6 +1,2 @@
 # edit this file according to your installation
 # changes are ignored by git and won't be commited
-
-# TXMLParser installation (headers + library)
-export TXMLPARSER=/home/vladyslav/git_repositories/TXMLParser
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$TXMLPARSER/lib
