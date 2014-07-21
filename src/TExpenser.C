7ad35fd04d2a2e18e07bb52fe937a47fb661e612
Vladyslav Libov <vladyslav@linux-mebx>
2014-07-21 21:53:57 +0200

    added a new category

--- src/TExpenser.C
+++ src/TExpenser.C
@@ -43,8 +43,8 @@ TString toStr(Int_t arg) {
 }
 
 // constants
-const unsigned NCATEGORIES = 10;
-TString CATEGORIES[]  = {"Food", "Restaurant", "Flat", "Cash", "Music equipment", "Books", "Clothes", "Presents", "Travel", "Other"};
+const unsigned NCATEGORIES = 11;
+TString CATEGORIES[]  = {"Food", "Restaurant", "Flat", "Cash", "Music equipment", "Books", "Clothes", "Presents", "Travel", "Transport HH", "Other"};
 const TString MONTHS[12]={"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
 const unsigned FIRST_YEAR = 2009;
 const unsigned LAST_YEAR = 2015;
